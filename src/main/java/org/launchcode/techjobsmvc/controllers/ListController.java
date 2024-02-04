61add7511f60275085b3a169e75961637e9f54f5
Daniyal Syed <daniyal.syed@live.com>
2024-02-03 22:55:27 -0600

    task 2 complete

--- src/main/java/org/launchcode/techjobsmvc/controllers/ListController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/ListController.java
@@ -28,6 +28,7 @@ public class ListController {
         columnChoices.put("positionType", "Position Type");
         columnChoices.put("coreCompetency", "Skill");
 
+        tableChoices.put("all", "View All");
         tableChoices.put("employer", JobData.getAllEmployers());
         tableChoices.put("location", JobData.getAllLocations());
         tableChoices.put("positionType", JobData.getAllPositionTypes());
@@ -47,7 +48,8 @@ public class ListController {
     }
 
     @GetMapping(value = "jobs")
-    public String listJobsByColumnAndValue(Model model, @RequestParam String column, @RequestParam(required = false) String value) {
+    public String listJobsByColumnAndValue(Model model, @RequestParam String column,
+                                                        @RequestParam(required = false) String value) {
         ArrayList<Job> jobs;
         if (column.equals("all")){
             jobs = JobData.findAll();
